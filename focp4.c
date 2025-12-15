447077c3c364c19f3fad9f5c0c9f80b0d485f8b4
Ananyajolly <77165074+Ananyajolly@users.noreply.github.com>
2025-12-15 19:30:33 +0530

    Add files via upload

---
+++ focp4.c
@@ -0,0 +1,48 @@
+/*#include<stdio.h>
+int main() {
+    int x, y, t;
+    printf("x is ");
+    scanf("%d",&x);
+    printf("y is ");
+    scanf("%d",&y);
+    t = x;
+    x = y;
+    y = t;
+    printf("\nnew x is %d", x);
+    printf("\nnew y is %d", y);
+    return 0;
+}*/
+/*arthmetic
+#include<stdio.h>
+int main() {
+    int m, n;
+    printf("m is ");
+    scanf("%d",&m);
+    printf("n is ");
+    scanf("%d",&n);
+    m = m + n;
+    n = m - n;
+    m = m - n;
+    printf("\nnew m is %d", m);
+    printf("\nnew n is %d", n);
+    return 0;
+}
+*/
+/*
+#include<stdio.h>
+void swap(int *p, int *q) {
+    int r;
+    r = *p;
+    *p = *q;
+    *q = r;
+}
+int main() {
+    int u = 7;
+    int v = 8;
+    swap(&u, &v);
+    printf("\n%d", u);
+    printf("\n%d", v);
+    return 0;
+}
+
+*/
